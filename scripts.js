fb2672c491ce218e99042617d9876cd0ffe6e4e5
Felipe Neves <feneves.fn@gmail.com>
2024-01-10 20:05:16 -0300

    projeto completo

---
+++ scripts.js
@@ -0,0 +1,17 @@
+    var nomecompleto = "Felipe Neves"
+    var anocorrente= "2023"
+    var twiter = "@meuTwitter"
+    var insta = "@MeuInsta"
+    var facebook = "@MeuFace"
+    var LinkedIn = "@Linkedin"
+    var sexo ="masculino"
+    var polo = "São Paulo - Higienopolis" 
+    document.write(nomecompleto+
+         "<br>" +anocorrente+
+         "<br>" +twiter+
+         "<br>" +insta+
+         "<br>" +facebook+
+         "<br>" +LinkedIn+
+         "<br>" +sexo+
+         "<br>" +polo);
+ 
